a25935196578656fa33a948d4f22140a40dc2101
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-07-14 11:06:41 +0200

    wip: moving gather related artifacts out

--- scripts/setup_ckan.sh
+++ scripts/setup_ckan.sh
@@ -35,7 +35,7 @@ pushd ckan
 }
 
 docker-compose -f docker-compose.yml up -d
-until docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add admin | tee ../creds.txt && echo "done"
+until docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add admin | tee creds.txt && echo "done"
 do
     echo "waiting for ckan container to be ready..."
     sleep 5
