1027b95c37718f0926e38a106e6bbf09a0b2f19f
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2019-05-15 22:50:12 +0100

    wip: fixing asset registration. Still have csfr issue with client

--- assets/generation/register.py
+++ assets/generation/register.py
@@ -23,19 +23,23 @@ import json
 import os
 import sys
 
+
 def env(key):
     return os.environ.get(key, None)
+
 
 PROJECT_NAME = env('PROJECT_NAME')
 SUBMISSION_ENDPOINT = env('SUBMISSION_ENDPOINT')
 kernel_url = env('KERNEL_URL')
 user = env('KERNEL_USER')
 pw = env('KERNEL_PASSWORD')
+REALM = env('REALM')
 
 try:
-    client = Client(kernel_url, user, pw)
+    client = Client(kernel_url, user, pw, log_level='DEBUG', realm=REALM)
 except Exception as err:
-    print("Kernel is not ready. Please check it's status or wait a moment and try again : %s" % err)
+    print(f'Kernel is not ready. Please check '
+          f'''it's status or wait a moment and try again : {err}''')
     sys.exit(1)
 
 
@@ -43,25 +47,28 @@ def file_to_json(path):
     with open(path) as f:
         return json.load(f)
 
+
 def pprint(obj):
     print(obj)
 
+
 def register_project():
-    project_obj = { "name": PROJECT_NAME }
+    project_obj = {'name': PROJECT_NAME}
     return client.projects.create(data=project_obj)
+
 
 def schema():
     schema_names = []
     schemas = []
-    defs = file_to_json("/code/assets/schemas/all.json")
+    defs = file_to_json('/code/assets/schemas/all.json')
     for obj in defs:
         name = obj.get('name')
         schema_names.append(name)
         schema_obj = {
-            "name": name,
-            "type": "record",
-            "revision": "1",
-            "definition": obj
+            'name': name,
+            'type': 'record',
+            'revision': '1',
+            'definition': obj
         }
         schemas.append(schema_obj)
     out = []
@@ -78,34 +85,38 @@ def project_schema(project, ids):
     out = {}
     for name in ids.keys():
         project_schema_obj = {
-            "revision": "1",
-            "name": name,
-            "schema": ids[name],
-            "project": project,
-            "masked_fields": "[]",
-            "transport_rule": "[]",
-            "mandatory_fields": "[]"
+            'revision': '1',
+            'name': name,
+            'schema': ids[name],
+            'project': project,
+            'masked_fields': '[]',
+            'transport_rule': '[]',
+            'mandatory_fields': '[]'
         }
         out[name] = client.projectschemas.create(data=project_schema_obj)
     return out
 
+
 def mappingset(project):
-    return client.mappingsets.create(data={
-            "project": project,
-            "name": "default_set"}
-        )
+    return client.mappingsets.create(
+        data={
+            'project': project,
+            'name': 'default_set'}
+    )
+
 
 def mapping(project, mapping_set_id, ids):
-    mapping_def = file_to_json("/code/assets/schemas/mapping.json")
+    mapping_def = file_to_json('/code/assets/schemas/mapping.json')
     mapping_obj = {
-        "name": SUBMISSION_ENDPOINT,
-        "definition": {"mapping": mapping_def},
-        "revision": "1"
+        'name': SUBMISSION_ENDPOINT,
+        'definition': {'mapping': mapping_def},
+        'revision': '1'
     }
     mapping_obj['definition']['entities'] = ids
     mapping_obj['mappingset'] = mapping_set_id
     mapping_obj['project'] = project
     return client.mappings.create(data=mapping_obj)
+
 
 def register():
     project = register_project()
@@ -115,23 +126,27 @@ def register():
         sys.exit(0)
     schema_info = schema()
     pprint(schema_info)
-    schema_ids = {obj.name : obj.id for obj in schema_info}
+    schema_ids = {obj.name: obj.id for obj in schema_info}
     pprint(schema_ids)
     project_schemas = project_schema(project_id, schema_ids)
     pprint(project_schemas)
-    ps_ids = {ps.name : ps.id for ps in project_schemas.values()}
+    ps_ids = {ps.name: ps.id for ps in project_schemas.values()}
     ms = mappingset(project_id)
     sub_id = mapping(project_id, ms.id, ps_ids)
     pprint(sub_id)
-    entity_references = sub_id.definition.get('entities')
+    # entity_references = sub_id.definition.get('entities')
 
 
-if __name__ == "__main__":
-    reqs = ['KERNEL_URL' , 'KERNEL_USER', 'KERNEL_PASSWORD', 'SUBMISSION_ENDPOINT', 'PROJECT_NAME']
+if __name__ == '__main__':
+    reqs = [
+        'KERNEL_URL',
+        'KERNEL_USER',
+        'KERNEL_PASSWORD',
+        'SUBMISSION_ENDPOINT',
+        'PROJECT_NAME'
+    ]
     if False in [env(r) for r in reqs]:
-        log.error('Required Environment Variable is missing.')
-        log.error('Required: %s' % (reqs,))
+        print('Required Environment Variable is missing.')
+        print('Required: %s' % (reqs,))
         sys.exit(1)
     register()
-
-
