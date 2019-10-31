7fec7c9142aae2bf52bdfbed036dae6a1db2a065
obdulia <obdulia.losantos@gmail.com>
2019-10-31 10:55:27 +0100

    fix: simplify self-signed certificate

--- scripts/extras/setup_https.sh
+++ scripts/extras/setup_https.sh
@@ -25,97 +25,36 @@ source scripts/lib.sh || \
       exit 1 )
 source .env
 
-CERT_FOLDER="./.persistent_data/certs"
+CERT_FOLDER="./.certs"
 CERT_NAME="${CERT_FOLDER}/${BASE_DOMAIN}"
 
-function gen_local_cert {
-    mkdir -p ${CERT_FOLDER}
-    rm -Rf ${CERT_NAME}*
+mkdir -p ${CERT_FOLDER}
+rm -Rf ${CERT_NAME}*
 
-    echo_message "Generating self-signed certificate for ${BASE_DOMAIN}..."
-    openssl genrsa -out ${CERT_NAME}.key 4096
+echo_message "Generating self-signed certificate for ${BASE_DOMAIN}..."
+openssl genrsa -out ${CERT_NAME}.key 4096
 
-    openssl req -new \
-        -key ${CERT_NAME}.key \
-        -out ${CERT_NAME}.csr \
-        -subj "/O=eHealth Africa/OU=Aether Team/CN=${BASE_DOMAIN}"
+openssl req -new \
+    -key ${CERT_NAME}.key \
+    -out ${CERT_NAME}.csr \
+    -subj "/O=eHealth Africa/OU=Aether Team/CN=${BASE_DOMAIN}"
 
-    openssl x509 -req \
-        -days 365 \
-        -in ${CERT_NAME}.csr \
-        -signkey ${CERT_NAME}.key \
-        -out ${CERT_NAME}.crt
+openssl x509 -req \
+    -days 365 \
+    -in ${CERT_NAME}.csr \
+    -signkey ${CERT_NAME}.key \
+    -out ${CERT_NAME}.crt
 
-    # --------------------------------------------------------------------------
-    # workaround for self signed certificates
-    # include our certificate in the official certificate authority (CA) bundle
-    echo_message "Creating workaround for self-signed certificates and python containers..."
-    python3 -m pip install -q --upgrade --target=${CERT_FOLDER} certifi
-    PY_CERT="${CERT_FOLDER}/certifi/cacert.pem"
-    mv ${PY_CERT} ${PY_CERT}.original
-    cat ${CERT_NAME}.crt ${PY_CERT}.original > ${PY_CERT}
-    # --------------------------------------------------------------------------
+start_db
+start_auth_container kong
 
-    start_db
-    start_auth_container kong
+echo_message "Installing self-signed certificate in kong..."
+curl -s -i \
+    -X PUT "http://localhost:8001/certificates/00000000-0000-0000-0000-000000000000" \
+    -H 'Content-Type: application/json' \
+    -d "{\"cert\":\"$(cat ${CERT_NAME}.crt)\",\"key\":\"$(cat ${CERT_NAME}.key)\",\"snis\":[\"${BASE_DOMAIN}\"]}" \
+    --output "${CERT_NAME}-kong.log" \
+    --write-out '%{http_code}'
 
-    echo_message "Installing self-signed certificate in kong..."
-    curl -i -X PUT "http://localhost:8001/certificates/00000000-0000-0000-0000-000000000000" \
-        -H 'Content-Type: application/json' \
-        -d "{\"cert\":\"$(cat ${CERT_NAME}.crt)\",\"key\":\"$(cat ${CERT_NAME}.key)\",\"snis\":[\"${BASE_DOMAIN}\"]}"
-}
-
-function instructions {
-    cat << EOF
-
---------------------------------------------------------------------------------
-
-The self-signed certificate for ${BASE_DOMAIN} was included in the certificates
-list stored in [${PY_CERT}] and installed in kong.
-Please follow the next points to finish the local setup.
-
---------------------------------------------------------------------------------
-
-1. Change in your [.env] file the following:
-
-BASE_PROTOCOL=https
-CERT_FOLDER=${CERT_FOLDER}
-KEYCLOAK_SERVER_URL=https://${BASE_DOMAIN}/auth/realms
-
---------------------------------------------------------------------------------
-
-2. Add in your auth/docker-compose.yml file in the kong service the following:
-
-  kong:
-    environment:
-      KONG_PROXY_LISTEN: 0.0.0.0:80, ssl 0.0.0.0:443
-    ports:
-      - 443:443
-
---------------------------------------------------------------------------------
-
-3. Add to your {module}/docker-compose.yml file the following volume entry
-in each service that uses https to communicate internally with
-the rest of services (kernel, odk, ui, gather):
-
-    volumes:
-      # -------------------------------------------------------------
-      # DO NOT USE THIS WORKAROUND IN ANY PRODUCTION ENVIRONMENT!!!
-      - ${CERT_FOLDER}/certifi:/usr/local/lib/python3.7/site-packages/certifi
-      # -------------------------------------------------------------
-
---------------------------------------------------------------------------------
-
-OPTIONAL
-
-4. Install the [${BASE_DOMAIN}.crt] file in your Android device certificates list
-Follow this link instructions: https://support.google.com/nexus/answer/2844832
-Maybe you'll need to reboot the device afterwards.
-Most probably this doesn't work at all.
-
---------------------------------------------------------------------------------
-EOF
-}
-
-gen_local_cert
-instructions
+echo ""
+echo_success "Done!"
